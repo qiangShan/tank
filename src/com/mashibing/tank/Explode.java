8ce75b57da84016c91356cd8d0b97784ee5efe61
qiangShan <1059931090@qq.com>
2023-02-19 20:35:37 +0800

    Initial commit

---
+++ src/com/mashibing/tank/Explode.java
@@ -0,0 +1,50 @@
+package com.mashibing.tank;
+
+import com.mashibing.factory.BaseExplode;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Explode extends BaseExplode {
+    private int x,y;
+    public static int WIDTH=ResourceMgr.explodes[0].getWidth();
+    public static int HEIGHT=ResourceMgr.explodes[0].getHeight();
+    private TankFrame tankFrame=null;
+    //private boolean living=true;
+    private int step=0;
+
+
+
+    public Explode(int x, int y, TankFrame tankFrame) {
+        this.x = x;
+        this.y = y;
+        this.tankFrame=tankFrame;
+    }
+
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void paint(Graphics graphics){
+        graphics.drawImage(ResourceMgr.explodes[step++],x,y,null);
+        if(step>=ResourceMgr.explodes.length){
+            tankFrame.explodes.remove(this);
+        }
+    }
+
+
+}
